8a0bd7bb18a28ba79eca33902ddcc16f2cc307da
hcand <hilmican_deliktas@hotmail.com>
2025-05-15 15:16:41 +0300

    ipyleaflet added as Map class

--- hilgeodev/hilgeodev.py
+++ hilgeodev/hilgeodev.py
@@ -1,1 +1,9 @@
 """Main module."""
+import ipyleaflet
+
+class Map(ipyleaflet.Map):
+    def __init__(self, center = (39,36), **kwargs):
+        super().__init__(center=center, **kwargs)
+
+    def add_base_map(self):
+        return self.add_layer(ipyleaflet.basemaps.OpenStreeMap.Mapnik)
