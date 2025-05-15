ba211ab4f3f6add97c81f265f44a3239d51b2fad
hcand <hilmican_deliktas@hotmail.com>
2025-05-15 17:01:37 +0300

    added add_geojson function

--- hilgeodev/hilgeodev.py
+++ hilgeodev/hilgeodev.py
@@ -1,9 +1,20 @@
 """Main module."""
 import ipyleaflet
 
+
 class Map(ipyleaflet.Map):
     def __init__(self, center = (39,36), **kwargs):
         super().__init__(center=center, **kwargs)
 
     def add_base_map(self):
         self.add_layer(ipyleaflet.basemaps.OpenStreeMap.Mapnik)
+
+    def add_geojson(self, url):
+        import geopandas as gpd
+        gdf = gpd.read_file(url)
+        data = gdf.__geo_interface__ 
+        gj = ipyleaflet.GeoJSON(data=data)
+        bounds = gdf.total_bounds
+        self.add_layer(gj)
+        self.fit_bounds([[bounds[1],bounds[0]],[bounds[3],bounds[2]]])
+
